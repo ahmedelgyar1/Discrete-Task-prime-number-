a1fa220e7b788b53e17de0255e2ca880bf853efa
ahmedelgyar1 <63425509+ahmedelgyar1@users.noreply.github.com>
2022-12-29 20:10:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_number_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the frist number");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int end = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = start; i <= end; i++)
+            {
+                for (int x = 1; x <= i; x++)
+                {
+                    if (i % x == 0)
+                        count++;
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine(i);
+                    count = 0;
+                }
+                else
+                {
+                    count = 0;
+                }
+
+            }
+        }
+    }
+}
